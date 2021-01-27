857c88edce4363eb9300dabd8cafad77c004aacb
Alex <agasparr98@gmail.com>
2021-01-27 11:04:12 +0100

    exo fini

---
+++ main.js
@@ -0,0 +1,9 @@
+let tartine = document.getElementsByClassName('tartine')[0].textContent;
+console.log(tartine);
+
+let toast = document.getElementById('toast').textContent;
+console.log(toast);
+
+let tartineLast = document.getElementsByClassName('tartine')[document.getElementsByClassName('tartine').length - 1];
+console.log(tartineLast);
+
